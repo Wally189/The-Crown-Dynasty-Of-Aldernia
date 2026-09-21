8fc35b33cd0b36f3dc2269d096e69b57c1e66197
Alan Gallagher <53058801+Wally189@users.noreply.github.com>
2026-09-21 05:42:01 +0100

    Website cohesion pass 1c: make national identity first-paint and add journey system

--- assets/site.js
+++ assets/site.js
@@ -1,9 +1,5 @@
 (() => {
   'use strict';
-
-  if (window.location.pathname.includes('/country/')) {
-    document.body.classList.add('country-surface');
-  }
   const script = document.currentScript;
   const root = script?.dataset.root || './';
   const londonTime = new Intl.DateTimeFormat('en-GB', {
